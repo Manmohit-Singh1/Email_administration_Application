137f4972d798d723774a4b7e068c0f8749da408d
Manmohit-Singh1 <manmohit.singh.raina@gmail.com>
2024-04-11 19:44:55 +0530

    Initial commit

---
+++ src/Email_App/Email_app.java
@@ -0,0 +1,9 @@
+package Email_App;
+
+public class Email_app {
+
+    public static void main(String[] args){
+        email em1 = new email("John", "Smith");
+        System.out.println(em1.showInfo());
+    }
+}
